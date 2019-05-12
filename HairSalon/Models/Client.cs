4d543070ebb795ab42e55dbda038da8973fa0b0f
Stephen Trewick <stephent818@gmail.com>
2019-05-11 18:17:44 -0700

    added DBConfiguration and DB.cs

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -38,30 +38,30 @@ namespace HairSalon.Models
       return _stylistId;
     }
 
-    // public static List<Client> GetAll()
-    // {
-    //   List<Client> allClients = new List<Client>{};
-    //   MySqlConnection conn = DB.Connection();
-    //   conn.Open();
-    //   var cmd = conn.CreateCommand() as MySqlCommand;
-    //   cmd.CommandText = @"SELECT * FROM clients;";
-    //   var rdr = cmd.ExecuteReader() as MySqlDataReader;
-    //
-    //   while (rdr.Read())
-    //   {
-    //     int clientId = rdr.GetInt32(0);
-    //     string clientName = rdr.GetString(1);
-    //     int stylistId = rdr.GetInt32(2);
-    //     Client newClient = new Client(clientName, stylistId, clientId);
-    //     allClients.Add(newClient);
-    //     }
-    //     conn.Close();
-    //     if (conn != null)
-    //     {
-    //       conn.Dispose();
-    //     }
-    //     return allClients;
-    // }
+    public static List<Client> GetAll()
+    {
+      List<Client> allClients = new List<Client>{};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM clients;";
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+
+      while (rdr.Read())
+      {
+        int clientId = rdr.GetInt32(0);
+        string clientName = rdr.GetString(1);
+        int stylistId = rdr.GetInt32(2);
+        Client newClient = new Client(clientName, stylistId, clientId);
+        allClients.Add(newClient);
+        }
+        conn.Close();
+        if (conn != null)
+        {
+          conn.Dispose();
+        }
+        return allClients;
+    }
 
     // public static void ClearAll()
     // {
--- HairSalon/Startup.cs
+++ HairSalon/Startup.cs
@@ -44,7 +44,7 @@ namespace HairSalon
 
   public static class DBConfiguration
   {
-    public static string ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=heather_zurek;";
+    public static string ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=stephen_trewick;";
   }
 
 }
